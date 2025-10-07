39812dd9f171c810892c1f2e496fabdd28d75669
Lestlin Robins <lestlinr@gmail.com>
2025-10-07 12:38:55 +0530

    fixed stuff

--- src/components/KnowledgeScreen.tsx
+++ src/components/KnowledgeScreen.tsx
@@ -12,7 +12,6 @@ import {
 } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
-//test
 interface KnowledgeScreenProps {
   onBack?: () => void;
 }
